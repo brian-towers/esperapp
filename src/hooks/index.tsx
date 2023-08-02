2b71b23a8d5852e085aaed7c3c354f2a4ed5b252
Brian Towers <btowers@netrixllc.com>
2023-08-01 21:10:04 -0300

    route guard

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,6 +1,5 @@
-import useApi from "./useApi";
-import useAppDispatch from "./useAppDispatch";
-import useAppSelector from "./useAppSelector";
-import useAuthentication from "./useAuthentication";
+import useAppDispatch from './useAppDispatch';
+import useAppSelector from './useAppSelector';
+import useAuthentication from './useAuthentication';
 
-export { useApi, useAuthentication, useAppSelector, useAppDispatch };
+export { useAuthentication, useAppSelector, useAppDispatch };
