86b17a1b728dd9e03c3aeb151cf200c1d1ba8299
Brian Towers <btowers@netrixllc.com>
2023-08-11 18:54:11 -0300

    stepper + illustrations

--- src/pages/InitialConfig/InitialConfig.tsx
+++ src/pages/InitialConfig/InitialConfig.tsx
@@ -24,6 +24,12 @@ interface Business {
 }
 
 export default function InitialConfig() {
+  const [steps, setSteps] = useState([
+    { id: '01', name: 'Nombre del establecimiento', href: '#', status: 'complete' },
+    { id: '02', name: 'Mesas disponibles', href: '#', status: 'upcoming' },
+    { id: '03', name: 'Estados de las mesas', href: '#', status: 'upcoming' },
+    { id: '04', name: 'Miembros del equipo', href: '#', status: 'upcoming' }
+  ]);
   const [currentStep, setCurrentStep] = useState(steps[0]);
   const [business, setBusiness] = useState<Business>({
     name: '',
@@ -32,9 +38,43 @@ export default function InitialConfig() {
     collaborators: []
   });
 
-  const handleNext = () => {};
+  const handleNext = () => {
+    const currentIndex = steps.findIndex((step) => step.id === currentStep.id);
+    if (currentIndex < steps.length - 1) {
+      const nextStep = steps[currentIndex + 1];
+      setCurrentStep(nextStep);
+      // Update step statuses (optional, based on your logic)
+      const updatedSteps = steps.map((step) => {
+        if (step.id === nextStep.id) {
+          return { ...step, status: 'current' };
+        } else if (step.id === currentStep.id) {
+          return { ...step, status: 'complete' };
+        } else {
+          return step;
+        }
+      });
+      setSteps(updatedSteps);
+    }
+  };
 
-  const handleBack = () => {};
+  const handleBack = () => {
+    const currentIndex = steps.findIndex((step) => step.id === currentStep.id);
+    if (currentIndex > 0) {
+      const prevStep = steps[currentIndex - 1];
+      setCurrentStep(prevStep);
+      // Update step statuses (optional, based on your logic)
+      const updatedSteps = steps.map((step) => {
+        if (step.id === prevStep.id) {
+          return { ...step, status: 'current' };
+        } else if (step.id === currentStep.id) {
+          return { ...step, status: 'upcoming' };
+        } else {
+          return step;
+        }
+      });
+      setSteps(updatedSteps);
+    }
+  };
 
   const handleReset = () => {};
 
@@ -48,32 +88,43 @@ export default function InitialConfig() {
   return (
     <>
       <Stepper steps={steps} />
-      <div className="flex w-full justify-center ">
-        <div className="flex min-h-full w-1/3 flex-col justify-center px-6 py-12 lg:px-8 space-y-12">
-          <StepOne />
-          <div className="flex justify-center">
-            {/* <button
-              type="submit"
-              className="flex max-w-sm text-orange-600 border border-orange-600 rounded-md justify-center font-semibold px-3 py-1.5 text-sm leading-6 "
-            >
-              {'Atras'}
-            </button> */}
+      <div className="flex w-full h-screen justify-center ">
+        <div className="w-1/3 flex flex-col justify-between h-1/2 px-6 lg:px-8 space-y-12">
+          <div className="flex justify-center mt-8 mb-1">
+            <div className="flex-col">
+              {currentStep.id === '01' && <StepOne />}
+              {currentStep.id === '02' && <StepTwo />}
+              {currentStep.id === '03' && <StepThree handleTableStatus={handleTableStatus} />}
+              {currentStep.id === '04' && <StepFour />}
+            </div>{' '}
+          </div>
+          <div className="flex justify-center space-x-6">
+            {currentStep.id !== '01' && (
+              <button
+                onClick={handleBack}
+                className="flex max-w-sm text-orange-600 border border-orange-600 rounded-md justify-center font-semibold px-3 py-1.5 text-sm leading-6"
+                style={{ width: '120px' }}
+              >
+                {'Atras'}
+              </button>
+            )}
             <button
-              type="submit"
+              onClick={handleNext}
               className="inline-flex items-center max-w-sm text-white bg-orange-600 hover:bg-orange-500 rounded-md font-semibold px-3 py-1.5 text-sm leading-6"
+              style={{ width: '120px' }}
             >
               {'Siguiente'}
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
                 className="w-6 h-6 ml-2"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
@@ -87,91 +138,92 @@ export default function InitialConfig() {
 
 const StepOne = () => {
   return (
-    <div className="flex justify-center my-8">
-      <div className="flex-col">
-        <div className="sm:mx-auto sm:w-full flex justify-center sm:max-w-sm">
-          <h1 className="text-7xl font-quicksand">¡Hola!</h1>
-        </div>
-        <p className="block text-2xl leading-6 text-gray-900 mt-8 ">Indicanos el nombre de tu establecimiento</p>
-        <div className="flex justify-center mt-8">
-          <input
-            id="establecimiento"
-            type="establecimiento"
-            autoComplete="establecimiento"
-            required
-            className="block w-2/3 rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-orange-600 sm:text-sm sm:leading-6"
-          />
-        </div>
+    <>
+      <div className="sm:mx-auto sm:w-full flex justify-center sm:max-w-sm">
+        <h1 className="text-7xl font-quicksand">¡Hola!</h1>
       </div>
-    </div>
+      <p className="block text-2xl leading-6 text-gray-900 mt-8 ">Indicanos el nombre de tu establecimiento</p>
+      <div className="flex justify-center mt-8">
+        <input
+          id="establecimiento"
+          type="establecimiento"
+          autoComplete="establecimiento"
+          required
+          className="block w-2/3 rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-orange-600 sm:text-sm sm:leading-6"
+        />
+      </div>
+    </>
   );
 };
 
 const StepTwo = () => {
   return (
-    <div className="flex justify-center my-8">
-      <div className="flex-col  mt-8">
-        <label htmlFor="tables" className="block text-xl leading-6 text-gray-900">
-          Cantidad de mesas disponibles
-        </label>
-        <div className="flex justify-center mt-4">
-          <input
-            id="tables"
-            type="tables"
-            autoComplete="tables"
-            required
-            className="block w-2/3 rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-orange-600 sm:text-sm sm:leading-6"
-          />
-        </div>
+    <>
+      <div className="flex justify-center mb-4">
+        <img className="w-auto h-44" src="tables.svg" alt="" />
       </div>
-    </div>
+      <label htmlFor="tables" className="block text-xl leading-6 text-gray-900">
+        Cantidad de mesas disponibles
+      </label>
+      <div className="flex justify-center mt-4">
+        <input
+          id="tables"
+          type="tables"
+          autoComplete="tables"
+          required
+          className="block w-2/3 rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-orange-600 sm:text-sm sm:leading-6"
+        />
+      </div>
+    </>
   );
 };
 
 const StepThree = ({ handleTableStatus }: { handleTableStatus: (status: string) => void }) => {
   return (
-    <div className="flex justify-center my-8">
-      <div className="flex-col  mt-8">
-        <label htmlFor="tables" className="block text-xl leading-6 text-gray-900">
-          Seleccione los estados posibles de las mesas
-        </label>
-        <div className="flex justify-center mt-4">
-          <div className="grid grid-cols-3 gap-4">
-            {tableStatusOptions.map((status) => {
-              return (
-                <button
-                  onClick={() => handleTableStatus(status)}
-                  className="flex w-full text-white bg-orange-600 hover:bg-orange-500 rounded-md justify-center font-semibold px-3 py-1.5 text-sm leading-6 shadow-md"
-                  key={status} // Add a unique key for the button
-                >
-                  {status}
-                </button>
-              );
-            })}
-          </div>
+    <>
+      <div className="flex justify-center mb-4">
+        <img className="w-auto h-44" src="status.svg" alt="" />
+      </div>
+      <label htmlFor="tables" className="block text-xl leading-6 text-gray-900">
+        Seleccione los estados posibles de las mesas
+      </label>
+      <div className="flex justify-center mt-4">
+        <div className="grid grid-cols-3 gap-4">
+          {tableStatusOptions.map((status) => {
+            return (
+              <button
+                onClick={() => handleTableStatus(status)}
+                className="flex w-full text-white bg-orange-600 hover:bg-orange-500 rounded-md justify-center font-semibold px-3 py-1.5 text-sm leading-6 shadow-md"
+                key={status} // Add a unique key for the button
+              >
+                {status}
+              </button>
+            );
+          })}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
 const StepFour = () => {
   return (
-    <div className="flex justify-center my-8">
-      <div className="flex-col  mt-8">
-        <label htmlFor="tables" className="block text-xl leading-6 text-gray-900">
-          Agregue a los miembros de su equipo
-        </label>
-        <div className="flex justify-center mt-4">
-          <input
-            id="tables"
-            type="tables"
-            autoComplete="tables"
-            required
-            className="block w-2/3 rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-orange-600 sm:text-sm sm:leading-6"
-          />
-        </div>
+    <>
+      <div className="flex justify-center mb-4">
+        <img className="w-auto h-44" src="team.svg" alt="" />
       </div>
-    </div>
+      <label htmlFor="tables" className="block text-xl leading-6 text-gray-900">
+        Agregue a los miembros de su equipo
+      </label>
+      <div className="flex justify-center mt-4">
+        <input
+          id="tables"
+          type="tables"
+          autoComplete="tables"
+          required
+          className="block w-2/3 rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-orange-600 sm:text-sm sm:leading-6"
+        />
+      </div>
+    </>
   );
 };
