f5251f10ef86e3ba5ce09e40e701b6b7d7f7198c
Brian Towers <btowers@netrixllc.com>
2023-08-12 15:56:39 -0300

    stepper

--- src/pages/InitialConfig/InitialConfig.tsx
+++ src/pages/InitialConfig/InitialConfig.tsx
@@ -76,7 +76,7 @@ export default function InitialConfig() {
     }
   };
 
-  const handleReset = () => {};
+  const handleSubmit = () => {};
 
   const handleTableStatus = (status: string) => {
     setBusiness((prevBusiness: Business) => ({
@@ -90,13 +90,13 @@ export default function InitialConfig() {
       <Stepper steps={steps} />
       <div className="flex w-full h-screen justify-center ">
         <div className="w-1/3 flex flex-col justify-between h-1/2 px-6 lg:px-8 space-y-12">
-          <div className="flex justify-center mt-8 mb-1">
+          <div className="flex justify-center mt-10 ">
             <div className="flex-col">
               {currentStep.id === '01' && <StepOne />}
               {currentStep.id === '02' && <StepTwo />}
               {currentStep.id === '03' && <StepThree handleTableStatus={handleTableStatus} />}
               {currentStep.id === '04' && <StepFour />}
-            </div>{' '}
+            </div>
           </div>
           <div className="flex justify-center space-x-6">
             {currentStep.id !== '01' && (
@@ -108,27 +108,52 @@ export default function InitialConfig() {
                 {'Atras'}
               </button>
             )}
-            <button
-              onClick={handleNext}
-              className="inline-flex items-center max-w-sm text-white bg-orange-600 hover:bg-orange-500 rounded-md font-semibold px-3 py-1.5 text-sm leading-6"
-              style={{ width: '120px' }}
-            >
-              {'Siguiente'}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-6 h-6 ml-2"
+            {currentStep.id !== '04' && (
+              <button
+                onClick={handleNext}
+                className="inline-flex items-center justify-between max-w-sm text-white bg-orange-600 hover:bg-orange-500 rounded-md font-semibold px-3 py-1.5 text-sm leading-6"
+                style={{ width: '120px' }}
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-            </button>
+                {'Siguiente'}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6 ml-2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </button>
+            )}
+            {currentStep.id == '04' && (
+              <button
+                onClick={handleSubmit}
+                className="inline-flex items-center justify-between max-w-sm text-white bg-orange-600 hover:bg-orange-500 rounded-md font-semibold px-3 py-1.5 text-sm leading-6"
+                style={{ width: '120px' }}
+              >
+                {'Finalizar'}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6 ml-2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </button>
+            )}
           </div>
         </div>
       </div>
@@ -139,11 +164,11 @@ export default function InitialConfig() {
 const StepOne = () => {
   return (
     <>
-      <div className="sm:mx-auto sm:w-full flex justify-center sm:max-w-sm">
+      <div className="flex justify-center items-center h-44 sm:mx-auto sm:w-full  mb-8 sm:max-w-sm">
         <h1 className="text-7xl font-quicksand">¡Hola!</h1>
       </div>
-      <p className="block text-2xl leading-6 text-gray-900 mt-8 ">Indicanos el nombre de tu establecimiento</p>
-      <div className="flex justify-center mt-8">
+      <p className="block text-xl leading-6 text-gray-900 mb-10">Indicanos el nombre de tu establecimiento</p>
+      <div className="flex justify-center mt-4">
         <input
           id="establecimiento"
           type="establecimiento"
@@ -159,12 +184,10 @@ const StepOne = () => {
 const StepTwo = () => {
   return (
     <>
-      <div className="flex justify-center mb-4">
-        <img className="w-auto h-44" src="tables.svg" alt="" />
+      <div className="flex justify-center mb-8">
+        <img className="w-auto h-44" src="tables3.svg" alt="" />
       </div>
-      <label htmlFor="tables" className="block text-xl leading-6 text-gray-900">
-        Cantidad de mesas disponibles
-      </label>
+      <p className="block text-xl leading-6 text-gray-900 mb-10">Cantidad de mesas disponibles</p>
       <div className="flex justify-center mt-4">
         <input
           id="tables"
@@ -181,13 +204,11 @@ const StepTwo = () => {
 const StepThree = ({ handleTableStatus }: { handleTableStatus: (status: string) => void }) => {
   return (
     <>
-      <div className="flex justify-center mb-4">
-        <img className="w-auto h-44" src="status.svg" alt="" />
+      <div className="flex justify-center mb-8">
+        <img className="w-auto h-44" src="status3.svg" alt="" />
       </div>
-      <label htmlFor="tables" className="block text-xl leading-6 text-gray-900">
-        Seleccione los estados posibles de las mesas
-      </label>
-      <div className="flex justify-center mt-4">
+      <p className="block text-xl leading-6 text-gray-900 mb-10">Seleccione los estados posibles de las mesas</p>
+      <div className="flex justify-center">
         <div className="grid grid-cols-3 gap-4">
           {tableStatusOptions.map((status) => {
             return (
@@ -209,12 +230,10 @@ const StepThree = ({ handleTableStatus }: { handleTableStatus: (status: string) 
 const StepFour = () => {
   return (
     <>
-      <div className="flex justify-center mb-4">
-        <img className="w-auto h-44" src="team.svg" alt="" />
+      <div className="flex justify-center mb-8">
+        <img className="w-auto h-44" src="team3.svg" alt="" />
       </div>
-      <label htmlFor="tables" className="block text-xl leading-6 text-gray-900">
-        Agregue a los miembros de su equipo
-      </label>
+      <p className="block text-xl leading-6 text-gray-900 mb-10">Agregue a los miembros de su equipo</p>
       <div className="flex justify-center mt-4">
         <input
           id="tables"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 export default {
   content: [
     "./index.html",
@@ -9,6 +10,18 @@ export default {
       fontFamily: {
         'quicksand': ['Quicksand', 'sans-serif']
       }
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      orange: colors.orange,
+      gray: colors.gray,
+      'white': '#ffffff',
+      'sage': '#C9CBA3',
+      'peach': '#FFE1A8',
+      'bitter': '#E26D5C',
+      'wine': '#723D46',
+      'dyke': '#472D30'
     },
   },
   plugins: ['@tailwindcss/forms'],
